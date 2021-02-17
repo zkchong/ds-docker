471a37595f8efe9b4ebb55af2b0ce0cbabcecb19
Zan Kai <zankai@axiatadigital.com>
2021-02-17 09:22:45 +0800

    Remove --priviledged in running docker. Use workaround.

--- redeploy.sh
+++ redeploy.sh
@@ -5,9 +5,12 @@ docker container rm ds_docker
 set -xe
 docker build  --rm -t ds_docker  -f ./Dockerfile  .
 
-HOST_PORT=8080 # Jupyter port.
+HOST_PORT=8081 # Jupyter port.
 WORKSPACE_PATH="/home/zankai/Dropbox/D03 Work"  # Change to your data path.
-docker run -i -t --privileged --name ds_docker \
+docker run -i -t --name ds_docker \
+    --device /dev/fuse \
+    --cap-add SYS_ADMIN \
+    --security-opt "apparmor=unconfined" \
     -v "$WORKSPACE_PATH":/data \
-    -p $HOST_PORT:8888 \
+    -p 25.41.122.211:$HOST_PORT:8888 \
     ds_docker 
