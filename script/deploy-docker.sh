478530089e5252d9741c5961f8399b9bd4156284
Zan Kai <zankai@axiatadigital.com>
2021-08-04 15:25:19 +0800

    Revise of deployment script and environment.yaml.

--- script/deploy-docker.sh
+++ script/deploy-docker.sh
@@ -1,16 +1,24 @@
-# Quick deploy
+
+set -x # Debug output
 docker container stop ds_docker # if it is still running.
 docker container rm ds_docker
 
-set -xe
+set -xe # Debug output and exit on error
 docker build  --rm -t ds_docker  -f ./Dockerfile  .
 
-HOST_PORT=8081 # Jupyter port.
-WORKSPACE_PATH="/home/zankai/axiata-vault"  # Change to your data path.
-docker run -i -t --name ds_docker \
+# Configure the followings.
+HOST_PORT=8890 # Jupyter port.
+WORKSPACE_PATH="/home/zankai/axiata-vault" # Change to your data path.
+AWS_PATH="/home/zankai/axiata-vault/.aws" # Change to your aws credential path.
+
+# Run
+docker run -i -t --rm  --name ds_docker \
     --device /dev/fuse \
     --cap-add SYS_ADMIN \
-    --security-opt "apparmor=unconfined" \
-    -v "$WORKSPACE_PATH":/code \
+    -v "$WORKSPACE_PATH":/data \
+    -v "$AWS_PATH":/home/ds_user/.aws \
     -p $HOST_PORT:8888 \
-    ds_docker 
+    -d \
+    ds_docker
+
+# "--device /dev/fuse" and "--cap-add SYS_ADMIN" are used to enable the fuse driver.
--- script/test-docker.sh
+++ script/test-docker.sh
@@ -1,18 +1,23 @@
-set -x
 
-
+set -x # Debug output
 docker container stop ds_docker # if it is still running.
 docker container rm ds_docker
 
+set -xe # Debug output and exit on error
 docker build  --rm -t ds_docker  -f ./Dockerfile  .
 
-HOST_PORT=8081 # Jupyter port.
-# WORKSPACE_PATH="/home/zankai/Dropbox/D03 Work" # Change to your data path.
-WORKSPACE_PATH="/home/zankai/axiata" # Change to your data path.
+# Configure the followings.
+HOST_PORT=8890 # Jupyter port.
+WORKSPACE_PATH="/home/zankai/axiata-vault" # Change to your data path.
+AWS_PATH="/home/zankai/axiata-vault/.aws" # Change to your aws credential path.
+
+# Run
 docker run -i -t --rm  --name ds_docker \
     --device /dev/fuse \
     --cap-add SYS_ADMIN \
-    --security-opt "apparmor=unconfined" \
-    -v "$WORKSPACE_PATH":/code \
+    -v "$WORKSPACE_PATH":/data \
+    -v "$AWS_PATH":/home/ds_user/.aws \
     -p $HOST_PORT:8888 \
-    ds_docker /bin/bash 
+    ds_docker bash
+
+# "--device /dev/fuse" and "--cap-add SYS_ADMIN" are used to enable the fuse driver.
