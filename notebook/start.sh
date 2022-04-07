db9d1a6dc13f6d3cbcfb470ee2bdf16b05377a80
azhar-ah-axiata <azhar.halim@myboost.co>
2022-04-07 12:38:38 +0800

    Update Jupyter Notebook initiation protocols

--- notebook/start.sh
+++ notebook/start.sh
@@ -2,10 +2,10 @@
 set -xe
  
 # Note that the external folder will be mount to this path.
-DATA_PATH=/data
+DATA_PATH="$HOME/.axiata"
 
 # Force notebook to start the shell withb bash.
-export SHELL=bash 
+export SHELL=bash
 
 # Start jupyter notebook.
 jupyter nbextension enable codefolding/main
@@ -16,10 +16,10 @@ jupyter nbextension enable collapsible_headings/main
 jupyter nbextension enable execute_time/ExecuteTime
 jupyter nbextension enable scroll_down/main
 jupyter nbextension enable notify/notify
+jupyter nbextension enable --py widgetsnbextension
 
 cd $DATA_PATH
-jupyter notebook \
-    --port 8888 \
-    --ip 0.0.0.0 \
-    --NotebookApp.token='' --no-browser
-
+export NB_USER="$(echo $USER | sed -r 's/CORP\\/ADS\\/g')"
+# export NB_USER="$(echo $NB_USER | sed -r 's/[\\\.]+//g')"
+export NB_USER="$(echo $NB_USER | sed -e 's/\(.*\)/\L\1/')"
+jupyter notebook --port 8888 --ip 0.0.0.0 --NotebookApp.token='' --no-browser
