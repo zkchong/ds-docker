31904fcc47b9f9fb0cfe19050a2faf8a8c530682
Zan Kai1~ <zkchong@gmail.com>
2023-02-04 18:15:44 +0800

    Change from docker to podman.

--- script/test-docker.sh
+++ script/test-docker.sh
@@ -1,34 +1,25 @@
-# Test docker
-set -x # Debug output
-docker_name=ds_docker  # Docker name
-
-# Clean previous docker if exists
-docker container stop $docker_name # if it is still running.
-docker container rm $docker_name
-
+# Running podman
 set -xe # Debug output and exit on error
-docker build  --rm -t $docker_name  -f ./Dockerfile  .
+
+DOCKER_NAME=ds_docker
+
+# Delete previous image if exists 
+[ "$(podman ps -a | grep $DOCKER_NAME)" ] && \
+    podman container stop $DOCKER_NAME  && \
+    podman container rm $DOCKER_NAME
+
+
+# Build the docker from scratch
+podman build  --rm -t $DOCKER_NAME  --format docker  -f ./Dockerfile  .
 
 #------------------------------------------------------------------------------
 # User Configuraiton
 #------------------------------------------------------------------------------
-HOST_PORT=8890 # Jupyter port. If using network=bridge, then it will be 8888.
-DATA_PATH="/home/zankai/axiata-vault" # Change to your data path.
-AWS_PATH="/home/zankai/axiata-vault/.aws" # Change to your aws credential path.
-MAP_DATA_PATH=/tmp/$docker_name/data # Do not change this.
-
-host_uid=`id -u`
-host_gid=`id -g`
-docker_uid=1000
-docker_gid=1000
-
-echo Binding "$DATA_PATH" to "$MAP_DATA_PATH" with sudo ...
-mkdir -p $MAP_DATA_PATH
-sudo bindfs -o nonempty --map=$host_uid/$docker_uid:@$host_gid/@$docker_gid  $DATA_PATH  $MAP_DATA_PATH
-
-# Run
-docker run -i -t --rm  --name $docker_name \
-    -v $AWS_PATH:/home/micromamba/.aws \
-    -v $MAP_DATA_PATH:/data \
+DATA_PATH="C:\Users\zkcho\OneDrive - Sunway Group\Documents\Projects" # Change to your data path.
+NOTEBOOK_SECRET_TOKEN='zankai123'
+# Run 
+podman run -i -t --rm  --name $DOCKER_NAME \
+    -v "$DATA_PATH":"/data" \
+    -e NOTEBOOK_SECRET_TOKEN="$NOTEBOOK_SECRET_TOKEN" \
     --network=host \
-    $docker_name bash
+    $DOCKER_NAME bash
