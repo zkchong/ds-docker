47f4abd792dac39073e5ccc67ed46065c8109975
Zan Kai <zkchong@gmail.com>
2023-03-24 08:38:54 +0800

    Fix WSLv2 error.

--- script/deploy-docker.sh
+++ script/deploy-docker.sh
@@ -15,8 +15,17 @@ podman build  --rm -t $DOCKER_NAME  --format docker  -f ./Dockerfile  .
 #------------------------------------------------------------------------------
 # User Configuraiton
 #------------------------------------------------------------------------------
+# DATA_PATH="/mnt/g/My Drive/G10 Projects/Projects" # Change to your data path.
 DATA_PATH="G:\My Drive\G10 Projects\Projects" # Change to your data path.
 NOTEBOOK_SECRET_TOKEN='zankai123' # Token to access the Jupyter notebook at first run.
+
+# Gonna run this 
+# podman machine start
+# https://github.com/microsoft/WSL/issues/2999#issuecomment-1377556365
+# Bug in WSL2 -- cannot detect G drive. Need to mount manually.
+podman machine ssh "sudo mount -t drvfs G: /mnt/g" # Force loading g drive.
+
+
 # Run 
 podman run -i -t --rm  --name $DOCKER_NAME \
     -v "$DATA_PATH":"/data" \
